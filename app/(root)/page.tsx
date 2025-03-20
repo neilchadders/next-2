8a9ccb63b8bf387e1cff10b6e534a6fff29e0c6c
neilchadders <neilchadders@hotmail.com>
2025-03-20 06:41:58 +0000

    add header and footer

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+//import ProductList from '@/components/shared/product/product-list';
+//import sampleData from '@/db/sample-data';
+
+const Homepage = () => {
+  return (
+    <>
+      Prostore
+    </>
+  );
+};
+
+export default Homepage;
