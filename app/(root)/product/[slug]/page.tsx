84eaf9ae75140c551adabdaac71688cec41a6308
neilchadders <neilchadders@hotmail.com>
2025-03-23 08:30:43 +0000

    run build-update product/slug/page

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import { getProductBySlug } from '@/lib/actions/product.actions';
 import { notFound } from 'next/navigation';
 import ProductPrice from '@/components/shared/product/product-price';
+import ProductImages from '@/components/shared/product/product-images';
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -18,7 +19,9 @@ const ProductDetailsPage = async (props: {
       <section>
         <div className='grid grid-cols-1 md:grid-cols-5'>
           {/* Images Column */}
-          <div className='col-span-2'>{/* Images Component */}</div>
+          <div className='col-span-2'>
+            <ProductImages images={product.images} />
+          </div>
           {/* Details Column */}
           <div className='col-span-2 p-5'>
             <div className='flex flex-col gap-6'>
@@ -26,11 +29,9 @@ const ProductDetailsPage = async (props: {
                 {product.brand} {product.category}
               </p>
               <h1 className='h3-bold'>{product.name}</h1>
-              
-               {/*
-               <p>
-               {product.rating} of {product.numReviews} Reviews }
-              </p> */}
+              <p>
+                {product.rating} of {product.numReviews} Reviews
+              </p>
               <div className='flex flex-col sm:flex-row sm:items-center gap-3'>
                 <ProductPrice
                   value={Number(product.price)}
