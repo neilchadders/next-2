40242ed014acb209e66771cc79e2b8b45c995749
Neil Chadwick <neilchadders@hotmail.com>
2025-06-12 15:02:21 +0100

    lesson 51-53 rmove from cart

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -6,9 +6,9 @@ import ProductPrice from '@/components/shared/product/product-price';
 import ProductImages from '@/components/shared/product/product-images';
 import AddToCart from '@/components/shared/product/add-to-cart';
 import { getMyCart } from '@/lib/actions/cart.actions';
-import ReviewList from './review-list';
+//import ReviewList from './review-list';
 import { auth } from '@/auth';
-import Rating from '@/components/shared/product/rating';
+//import Rating from '@/components/shared/product/rating';
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -38,8 +38,8 @@ const ProductDetailsPage = async (props: {
                 {product.brand} {product.category}
               </p>
               <h1 className='h3-bold'>{product.name}</h1>
-              <Rating value={Number(product.rating)} />
-              <p>{product.numReviews} reviews</p>
+             
+              
               <div className='flex flex-col sm:flex-row sm:items-center gap-3'>
                 <ProductPrice
                   value={Number(product.price)}
@@ -92,11 +92,7 @@ const ProductDetailsPage = async (props: {
       </section>
       <section className='mt-10'>
         <h2 className='h2-bold mb-5'>Customer Reviews</h2>
-        <ReviewList
-          userId={userId || ''}
-          productId={product.id}
-          productSlug={product.slug}
-        />
+       
       </section>
     </>
   );
