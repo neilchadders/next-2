8e208593667079714381b28d6f112562df94fa3e
Neil Chadwick <neilchadders@hotmail.com>
2025-06-12 12:34:09 +0100

    lesson 48 add to cart

--- lib/utils.ts
+++ lib/utils.ts
@@ -18,7 +18,7 @@ export function formatNumberWithDecimal(num: number): string {
 
 // Format errors
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function formatError(error: any) {
+export async function formatError(error: any) {
   if (error.name === 'ZodError') {
     // Handle Zod error
     const fieldErrors = Object.keys(error.errors).map(
@@ -38,5 +38,33 @@ export function formatError(error: any) {
     return typeof error.message === 'string'
       ? error.message
       : JSON.stringify(error.message);
+  }
+}
+
+// Round number to 2 decimal places
+export function round2(value: number | string) {
+  if (typeof value === 'number') {
+    return Math.round((value + Number.EPSILON) * 100) / 100;
+  } else if (typeof value === 'string') {
+    return Math.round((Number(value) + Number.EPSILON) * 100) / 100;
+  } else {
+    throw new Error('Value is not a number or string');
+  }
+}
+
+const CURRENCY_FORMATTER = new Intl.NumberFormat('en-US', {
+  currency: 'USD',
+  style: 'currency',
+  minimumFractionDigits: 2,
+});
+
+// Format currency using the formatter above
+export function formatCurrency(amount: number | string | null) {
+  if (typeof amount === 'number') {
+    return CURRENCY_FORMATTER.format(amount);
+  } else if (typeof amount === 'string') {
+    return CURRENCY_FORMATTER.format(Number(amount));
+  } else {
+    return 'NaN';
   }
 }
