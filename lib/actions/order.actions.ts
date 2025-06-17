97373f1caeaf6f378714b5bde069af17109e8013
Neil Chadwick <neilchadders@hotmail.com>
2025-06-17 18:24:09 +0100

    fix OrderDetailsTable to include Omit 3nd...

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -7,11 +7,11 @@ import { getMyCart } from './cart.actions';
 import { getUserById } from './user.actions';
 import { insertOrderSchema } from '../validators';
 import { prisma } from '@/db/prisma';
-import { CartItem, PaymentResult, ShippingAddress } from '@/types';
+import { CartItem, PaymentResult } from '@/types';
 import { paypal } from '../paypal';
 import { revalidatePath } from 'next/cache';
-import { PAGE_SIZE } from '../constants';
-import { Prisma } from '@prisma/client';
+//import { PAGE_SIZE } from '../constants';
+//import { Prisma } from '@prisma/client';
 //import { sendPurchaseReceipt } from '@/email';
 
 // Create order and create the order items
