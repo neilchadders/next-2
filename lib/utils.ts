a075dcc70a60fd6f98891c4727d63421210e7453
Neil Chadwick <neilchadders@hotmail.com>
2025-07-05 15:30:26 +0100

    add order history and user profile

--- lib/utils.ts
+++ lib/utils.ts
@@ -114,3 +114,28 @@ export const formatDateTime = (dateString: Date) => {
     timeOnly: formattedTime,
   };
 };
+
+// Form the pagination links
+export function formUrlQuery({
+  params,
+  key,
+  value,
+}: {
+  params: string;
+  key: string;
+  value: string | null;
+}) {
+  const query = qs.parse(params);
+
+  query[key] = value;
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query,
+    },
+    {
+      skipNull: true,
+    }
+  );
+}
